4c59271076b637ac401400e9b4a15a3e7ea7fd80
Pratik <pmprateekmohan@outlook.com>
2022-07-14 22:31:57 +0530

    Week1 basic types numbers

---
+++ basic_types/numbers/num1.rs
@@ -0,0 +1,11 @@
+// Remove something to make it work
+fn main() {
+    let _x: i32 = 5;
+    let mut _y: u32 = 5;
+
+//    y = x; type mismatch
+    
+    let _z = 10; // Type of z ? 
+
+    println!("Success!");
+}
