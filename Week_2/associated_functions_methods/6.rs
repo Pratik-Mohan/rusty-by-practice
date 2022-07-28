e608e06c4cee592832460ebcf39acb67981c0444
Pratik <pmprateekmohan@outlook.com>
2022-07-28 21:03:57 +0530

    Week_2 Assignment

---
+++ Week_2/associated_functions_methods/6.rs
@@ -0,0 +1,22 @@
+#[derive(Debug)]
+enum TrafficLightColor {
+    Red,
+    Yellow,
+    Green,
+}
+
+impl TrafficLightColor {
+    fn color(&self) -> String {
+        match *self {
+            TrafficLightColor::Red => "red".to_string(),
+            TrafficLightColor::Yellow => "yellow".to_string(),
+            TrafficLightColor::Green => "green".to_string(),
+        }
+    }
+}
+
+fn main() {
+    let c = TrafficLightColor::Yellow;
+    assert_eq!(c.color(), "yellow");
+    println!("{:?}", c);
+}
